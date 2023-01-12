b9954a2e90fc6c9b541a92ca0e978d7bae100bb0
Nikita Gil <nikitasaxar@gmail.com>
2023-01-12 18:19:54 +0200

    Add Driver class to Framework

---
+++ Framework/Driver.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using System;
+using System.Threading;
+using System.IO;
+
+namespace Framework
+{
+    internal class Driver
+    {
+        private static ThreadLocal<IWebDriver> driver = new ThreadLocal<IWebDriver>();
+
+        public static void Initialize()
+        {
+            driver.Value = new ChromeDriver();
+        }
+
+        public static IWebDriver GetDriver()
+        {
+            return driver.Value;
+        }
+
+        public static void OpenPage(string url)
+        {
+            driver.Value.Url = url;
+        }
+
+        public static void Quit()
+        {
+            driver.Value.Quit();
+        }
+
+        public static void TakeScreenshot(string testMethodName)
+        {
+            string screenshotsDirectoryPath = $"{AppDomain.CurrentDomain.BaseDirectory}/screenshots";
+            string screenshotName = $"{screenshotsDirectoryPath}\\scr{testMethodName}-{DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss")}.png";
+
+            Directory.CreateDirectory(screenshotsDirectoryPath);
+            Screenshot screenshot = ((ITakesScreenshot)driver.Value).GetScreenshot();
+            screenshot.SaveAsFile(screenshotName, ScreenshotImageFormat.Png);
+        }
+    }
+}
